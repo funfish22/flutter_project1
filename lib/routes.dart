57df40a2aa5083989daab8cd00d0ea5c9d53a958
funfish22 <j26391442@gmail.com>
2021-10-21 23:19:31 +0800

    新增兩種路由(不存在頁面、商品頁面)

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,7 @@
 import 'package:first_project/pages/home/index.dart';
 import 'package:first_project/pages/login.dart';
+import 'package:first_project/pages/not_found.dart';
+import 'package:first_project/pages/room_detail/index.dart';
 import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 
@@ -7,6 +9,7 @@ class Routes {
 // 1. 定義路由名稱
   static String home = '/';
   static String login = '/login';
+  static String roomDetail = '/room/:roomId';
 
 // 2. 定義路由處理函數
   static final Handler _homeHandler = Handler(
@@ -17,9 +20,19 @@ class Routes {
       handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
     return const LoginPage();
   });
+  static final Handler _roomDetailHandler = Handler(
+      handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
+    return RoomDetailPage(roomId: params['roomId']![0]);
+  });
+  static final Handler _notFoundHandler = Handler(
+      handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
+    return const NotFoundPage();
+  });
 // 3. 編寫函數 configurRoutes 關聯路由名稱和處理函數
   static void configureRoutes(FluroRouter router) {
     router.define(home, handler: _homeHandler);
     router.define(login, handler: _loginHandler);
+    router.define(roomDetail, handler: _roomDetailHandler);
+    router.notFoundHandler = _notFoundHandler;
   }
 }
--- lib/widgets/page_content.dart
+++ lib/widgets/page_content.dart
@@ -16,7 +16,10 @@ class PageContent extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           TextButton(onPressed: () {Navigator.pushNamed(context, Routes.home);}, child: const Text('首頁')),
-          TextButton(onPressed: () {Navigator.pushNamed(context, Routes.login);}, child: const Text('登入頁'))
+          TextButton(onPressed: () {Navigator.pushNamed(context, Routes.login);}, child: const Text('登入頁')),
+          TextButton(onPressed: () {Navigator.pushNamed(context, '/not');}, child: const Text('不存在的頁面')),
+          TextButton(onPressed: () {Navigator.pushNamed(context, '/room/電腦');}, child: const Text('商品, 名稱: 電腦')),
+          TextButton(onPressed: () {Navigator.pushNamed(context, '/room/手機');}, child: const Text('商品, 名稱: 手機'))
         ],
       ),
     );
